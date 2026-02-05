f4ce51dfe8f7afcea5ace5a8eafda307d4f73697
fkbm <1656911995@qq.com>
2026-02-05 17:31:36 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -47,7 +47,7 @@ IS_TERMUX=false
 PKG_INSTALL_CMD=""
 
 # Determine environment & package manager
-PKG_INSTALL_CMD="apt install -y"
+PKG_INSTALL_CMD="apt install"
 
 # -------------------------
 # Utility
